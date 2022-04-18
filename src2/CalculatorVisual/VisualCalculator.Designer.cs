54a02a9d2fe798c8da3fc197002588595584e592
Marina <marinaholodova927@gmail.com>
2022-04-18 22:15:13 +0300

    visual

---
+++ src2/CalculatorVisual/VisualCalculator.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CalculatorVisual;
+
+partial class VisualCalculator
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.calc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(16, 19);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(75, 23);
+            this.calc.TabIndex = 0;
+            this.calc.Text = "Посчитать";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calc);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private Button calc;
+}
---
+++ src2/CalculatorVisual/VisualCalculator.cs
@@ -0,0 +1,14 @@
+namespace CalculatorVisual;
+
+public partial class VisualCalculator : Form
+{
+    public VisualCalculator()
+    {
+        InitializeComponent();
+    }
+
+    private void calc_Click(object sender, EventArgs e)
+    {
+
+    }
+}
