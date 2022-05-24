82a5b40f3d1c39e5ed8e0107261f104243aef259
marina <marinaholodova927@gmail.com>
2022-05-24 22:16:05 +0300

    change

---
+++ src2/ClassOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassOOP.N_2_Train;
+using ClassOOP.N_3;
+using ClassOOP.Student;
+
+namespace ClassOOP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            IEntryPoint entryPoint;
+
+           // entryPoint = new StudentEntryPoint();
+          //entryPoint = new TrainEntryPoint();
+         entryPoint = new NumEntryPoint();
+            entryPoint.Start();
+        }
+    }
+}
