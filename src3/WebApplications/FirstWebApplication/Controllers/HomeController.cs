7b64927c234218b11c3d65791f415661a6ab4cda
marina <marinaholodova927@gmail.com>
2022-07-12 23:19:00 +0300

    wep app

---
+++ src3/WebApplications/FirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using FirstWebApplication.Models;
+
+namespace FirstWebApplication.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = DateTime.Now.ToString(CultureInfo.InvariantCulture)});
+    }
+}
